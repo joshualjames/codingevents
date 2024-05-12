47b178c51d1f3dc9da378b495c28b8c73451a260
Joshua James <josh452063@gmail.com>
2024-05-12 13:53:07 -0500

    almost finished, error with id incrementing by 2.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,19 +1,35 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 public class Event {
     //fields
     private int id;
     private static int nextId = 1;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max= 50, message="Name must be between 3 and 50 characters!")
     private String name;
+    @Size(max= 500, message="Description too long!")
     private String description;
+    @NotBlank(message = "Email is required")
+    @Email(message="Invalid Email!")
+    private String contactEmail;
 
 
     //Constructors
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
+        this();
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+    }
+
+    public Event(){
         this.id = nextId;
         nextId++;
     }
@@ -37,6 +53,14 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     //Methods
