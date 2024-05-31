884adb922ea70901b9f74808d31658660fb62643
Joshua James <josh452063@gmail.com>
2024-05-30 20:41:16 -0500

    finished abstract entity studio.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,27 +1,15 @@
 package org.launchcode.codingevents.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import jakarta.persistence.Entity;
-import java.util.Objects;
+import jakarta.validation.constraints.*;
 
 @Entity
-public class Event {
+public class Event extends AbstractEntity{
     //fields
-    @Id
-    @GeneratedValue
-    private int id;
 
     @NotBlank(message = "Location is required")
     private String location;
 
-    @NotBlank(message = "Name is required")
-    @Size(min = 3, max= 50, message="Name must be between 3 and 50 characters!")
-    private String name;
     @Size(max= 500, message="Description too long!")
     private String description;
     @NotBlank(message = "Email is required")
@@ -38,7 +26,7 @@ public class Event {
     //Constructors
     public Event(String name, String description, String contactEmail, String location,
                  boolean registration, int numberOfAttendees, EventType type) {
-        this.name = name;
+        super();
         this.description = description;
         this.contactEmail = contactEmail;
         this.location = location;
@@ -51,13 +39,6 @@ public class Event {
     public Event(){}
 
     //Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getDescription() {
         return description;
@@ -65,10 +46,6 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getContactEmail() {
@@ -111,22 +88,4 @@ public class Event {
         this.type = type;
     }
 
-    //Methods
-    @Override
-    public String toString() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
