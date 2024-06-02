795035bd0147a182fe2dee2855fbd69f006e6339
Joshua James <josh452063@gmail.com>
2024-06-02 12:18:28 -0500

    Created 'Delete Event Category.'

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("eventCategories")
@@ -46,5 +43,22 @@ public class EventCategoryController{
         return "redirect:/eventCategories";
     }
 
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("eventCategories", eventCategoryRepository.findAll());
+        return "eventCategories/delete";
+    }
 
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] categoryIDs) {
+
+        if (categoryIDs != null) {
+            for (int id : categoryIDs) {
+                eventCategoryRepository.deleteById(id);
+            }
+        }
+
+        return "redirect:/eventCategories";
+    }
 }
